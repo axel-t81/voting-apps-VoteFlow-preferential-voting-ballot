fdf9bcba135c0eb755ef159148d645daccdf5e80
Axel <63882492+axel-t81@users.noreply.github.com>
2024-02-08 17:48:02 +1100

    Create index.py
    Initial file. Using a user's chronological process to try and build it out the stages.

---
+++ index.py
@@ -0,0 +1,11 @@
+
+# Collect candidate information
+
+numberOfCandidates = input("How many candidates are running in the vote? ")
+float(numberOfCandidates)
+# Need to account for errors where people don't enter a number
+print()
+print("Okay, there will be " + numberOfCandidates + " candidates in this vote.")
+print()
+print("Let's collect each candidate's name...")
+print()
