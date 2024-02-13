6c5e663f59f84d8e285ed0c11cef9bb06000ace4
Axel <63882492+axel-t81@users.noreply.github.com>
2024-02-13 23:59:22 +1100

    Update index.py
    Basic functionality to collect number and names of candidates in vote.

--- index.py
+++ index.py
@@ -1,12 +1,22 @@
 
 # Collect candidate information
 
-numberOfCandidates = input("How many candidates are running in the vote? ")
-float(numberOfCandidates)
+numberOfCandidates = int(input("How many candidates are running in the vote? \n")) #Find out how many candidates
+number = str(numberOfCandidates) #Convert variable to a string so it can concatenate in the print statement below
+
 # Need to account for errors where people don't enter a number
-print()
-print("Okay, there will be " + numberOfCandidates + " candidates in this vote.")
-print()
-print("Let's collect each candidate's name...")
-print()
-# I dislike the usage of these print functions for new lines.
+
+print("Okay, there will be " + number + " candidates in this vote.\n") #Provide ffedback to user
+print("Let's collect each candidate's name...\n") #Prompt users next input
+
+candidate_names = list() #Create empty Python List that will stor all the str names within a single List object.
+
+i = 0 #I could only get it to work this way, ie using a While Loop, so I have set the counter to zero
+while i < numberOfCandidates:
+    content = input("What is the next candidates name? ")
+    candidate_names.append(content) #Using 'append' function to add each input as a value in the List
+    i = i+1 #Increment the counter for the While loop
+    print("Recorded & incremented\n") # Provide feedback to developer to know the Loop is looping
+
+print(candidate_names) # Provide feedback to developer to see what's now contained in the list
+    
